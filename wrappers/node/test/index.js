8cf84935cc48866cdfc7ce11c02f618e6a950597
Ant Cosentino <antony@mycosentino.com>
2017-08-07 16:23:48 +0200

    binding test set up

--- wrappers/node/test/index.js
+++ wrappers/node/test/index.js
@@ -4,6 +4,7 @@ var assert = require('assert')
 var blocked = require('blocked')
 
 var indy = require('..')
+var errors = indy.errors
 
 var block_timer = blocked(function(ms) {
   if (ms) {
@@ -11,3 +12,8 @@ var block_timer = blocked(function(ms) {
     process.exit(1)
   }
 }, {threshold: 60})
+
+var noop = function() {}
+var did = '00000000000000000000000'
+
+console.log(errors[indy.agent_remove_identity(0, 0, 0, did, noop)])
