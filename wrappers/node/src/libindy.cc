954d572e3d95ee427d4a8d69c41f561a6c037f36
Ant Cosentino <antony@mycosentino.com>
2017-08-16 14:14:44 +0200

    reorder method definitions

--- wrappers/node/src/libindy.cc
+++ wrappers/node/src/libindy.cc
@@ -4,6 +4,9 @@
 #include <indy_core.h>
 
 #include "napi_macros.h"
+#include "callback_hash.h"
+#include "napi_util.h"
+
 #include "agent.h"
 #include "signus.h"
 #include "anoncreds.h"
@@ -11,26 +14,13 @@
 #include "pool.h"
 #include "wallet.h"
 
-struct async_work_data {
-  char* indy_function_name;
-  indy_error_t err;
-  indy_handle_t listener_handle;
-  indy_handle_t command_handle;
-  indy_handle_t pool_handle;
-  indy_handle_t wallet_handle;
-  char* res;
-  napi_ref user_callback_ref;
-};
-
-typedef struct async_work_data async_work_data;
-
 void init(
   napi_env env,
   napi_value exports,
   napi_value module,
   void* priv
 ) {
-  printf("libindy init\n");
+  printf("TRACE init\n");
 
   napi_status status;
   napi_value fn;
@@ -53,21 +43,13 @@ void init(
   NAPI_DEFINE_METHOD("build_node_request", build_node_request);
   NAPI_DEFINE_METHOD("build_nym_request", build_nym_request);
   NAPI_DEFINE_METHOD("build_schema_request", build_schema_request);
-  NAPI_DEFINE_METHOD("close_pool_ledger", close_pool_ledger);
-  NAPI_DEFINE_METHOD("close_wallet", close_wallet);
   NAPI_DEFINE_METHOD("create_and_store_my_did", create_and_store_my_did);
-  NAPI_DEFINE_METHOD("create_pool_ledger_config", create_pool_ledger_config);
-  NAPI_DEFINE_METHOD("create_wallet", create_wallet);
   NAPI_DEFINE_METHOD("decrypt", decrypt);
-  NAPI_DEFINE_METHOD("delete_pool_ledger_config", delete_pool_ledger_config);
-  NAPI_DEFINE_METHOD("delete_wallet", delete_wallet);
   NAPI_DEFINE_METHOD("encrypt", encrypt);
   NAPI_DEFINE_METHOD("issuer_create_and_store_claim_def", issuer_create_and_store_claim_def);
   NAPI_DEFINE_METHOD("issuer_create_and_store_revoc_reg", issuer_create_and_store_revoc_reg);
   NAPI_DEFINE_METHOD("issuer_create_claim", issuer_create_claim);
   NAPI_DEFINE_METHOD("issuer_revoke_claim", issuer_revoke_claim);
-  NAPI_DEFINE_METHOD("open_pool_ledger", open_pool_ledger);
-  NAPI_DEFINE_METHOD("open_wallet", open_wallet);
   NAPI_DEFINE_METHOD("prover_create_and_store_claim_req", prover_create_and_store_claim_req);
   NAPI_DEFINE_METHOD("prover_create_master_secret", prover_create_master_secret);
   NAPI_DEFINE_METHOD("prover_create_proof", prover_create_proof);
@@ -76,8 +58,6 @@ void init(
   NAPI_DEFINE_METHOD("prover_get_claims_for_proof_req", prover_get_claims_for_proof_req);
   NAPI_DEFINE_METHOD("prover_store_claim", prover_store_claim);
   NAPI_DEFINE_METHOD("prover_store_claim_offer", prover_store_claim_offer);
-  NAPI_DEFINE_METHOD("refresh_pool_ledger", refresh_pool_ledger);
-  NAPI_DEFINE_METHOD("register_wallet_type", register_wallet_type);
   NAPI_DEFINE_METHOD("replace_keys", replace_keys);
   NAPI_DEFINE_METHOD("sign", sign);
   NAPI_DEFINE_METHOD("sign_and_submit_request", sign_and_submit_request);
@@ -85,6 +65,18 @@ void init(
   NAPI_DEFINE_METHOD("submit_request", submit_request);
   NAPI_DEFINE_METHOD("verifier_verify_proof", verifier_verify_proof);
   NAPI_DEFINE_METHOD("verify_signature", verify_signature);
+
+  NAPI_DEFINE_METHOD("close_wallet", close_wallet);
+  NAPI_DEFINE_METHOD("create_wallet", create_wallet);
+  NAPI_DEFINE_METHOD("delete_wallet", delete_wallet);
+  NAPI_DEFINE_METHOD("open_wallet", open_wallet);
+  NAPI_DEFINE_METHOD("register_wallet_type", register_wallet_type);
+
+  NAPI_DEFINE_METHOD("create_pool_ledger_config", create_pool_ledger_config);
+  NAPI_DEFINE_METHOD("close_pool_ledger", close_pool_ledger);
+  NAPI_DEFINE_METHOD("delete_pool_ledger_config", delete_pool_ledger_config);
+  NAPI_DEFINE_METHOD("refresh_pool_ledger", refresh_pool_ledger);
+  NAPI_DEFINE_METHOD("open_pool_ledger", open_pool_ledger);
 }
 
 NAPI_MODULE(libindy, init);
