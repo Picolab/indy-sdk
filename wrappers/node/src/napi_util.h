16d3991ac3e3363fdfbc5e68d249ac90c2ce603e
Ant Cosentino <antony@mycosentino.com>
2017-08-21 13:15:15 +0200

    add fixme note, remove unused includes, adds marshalling of boolean results, switching to condvar wait

--- wrappers/node/src/napi_util.h
+++ wrappers/node/src/napi_util.h
@@ -1,6 +1,5 @@
 
-// #include <time.h>
-// #include <errno.h>
+#include <thread>
 
 #include "napi.h"
 
@@ -22,21 +21,16 @@ size_t napi_string_length(napi_env env, napi_value string) {
 void execute(napi_env env, void* data) {
   indy_callback* callback = (indy_callback*) data;
   if (!callback) return;
-  while (callback) {
-    callback = (indy_callback*) data;
-    if (callback->completed == true) {
-      printf("====================================== COMPLETED %d\n", callback->handle);
-      break;
-    }
-    if (callback->called == true) {
-      printf("====================================== CALLED %d\n", callback->handle);
-      break;
-    }
-    if (callback->cancelled == true) {
-      printf("====================================== CANCELLED %d\n", callback->handle);
-      break;
-    }
-  }
+  printf("TRACE execute %d\n", callback->handle);
+  std::unique_lock<std::mutex> lock(callback->mutex);
+  callback->cv.wait(lock, [callback] {
+    return (
+      callback->completed ||
+      callback->called ||
+      callback->cancelled
+    );
+  });
+  lock.unlock();
 }
 
 void complete(
@@ -44,14 +38,13 @@ void complete(
   napi_status async_work_status,
   void* data
 ) {
-  printf("entering complete\n");
-
   indy_callback* callback = (indy_callback*) data;
-
-  if (callback == NULL) {
+  if (!callback) {
     printf("============================= NULL\n");
     return;
   }
+
+  printf("TRACE complete %d\n", callback->handle);
 
   if (async_work_status == napi_cancelled) {
     printf("============================ CANCELLED\n");
@@ -67,6 +60,7 @@ void complete(
   }
 
   if (callback->cancelled == true) {
+    printf("========================== CANCELLED %d\n", callback->handle);
     printf("========================== FREEING CALLBACK %d\n", callback->handle);
     free_callback(callback->handle);
     return;
@@ -151,12 +145,17 @@ void complete(
   }
   
   printf("before arg binding\n");
-  size_t total_args = callback->n_char_results + callback->n_handle_results;
+  size_t total_args = callback->n_char_results + callback->n_handle_results + callback->n_bool_results;
   size_t argc = 1 + total_args;
   napi_value argv[argc];
 
   argv[0] = err;
   size_t argc_idx = 1;
+
+  // FIXME
+  // it'll probably be necessary to switch to a dictionary of results
+  // as this method of binding results to the user-given callback is
+  // unlikely to be ordered correctly for every libindy call
 
   for (indy_handle_t handle : callback->handle_results) {
     napi_value res;
@@ -172,6 +171,18 @@ void complete(
     printf("attempting to bind arg %s\n", char_res);
     status = napi_create_string_utf8(env, char_res, strlen(char_res), &res);
     NAPI_CHECK_STATUS_VOID("napi_create_string_utf8");
+    argv[argc_idx] = res;
+    argc_idx += 1;
+  }
+
+  for (bool bool_result : callback->bool_results) {
+    napi_value num;
+    napi_value res;
+    printf("attempting to bind arg %s\n", bool_result);
+    status = napi_create_number(env, (double) bool_result, &num);
+    NAPI_CHECK_STATUS_VOID("napi_create_number");
+    status = napi_coerce_to_bool(env, num, &res);
+    NAPI_CHECK_STATUS_VOID("napi_coerce_to_bool");
     argv[argc_idx] = res;
     argc_idx += 1;
   }
