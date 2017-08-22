11637cc670b6abf0964e90f0aa2c2ba600ee50bd
Ant Cosentino <antony@mycosentino.com>
2017-08-22 15:37:39 +0200

    add usage of logging macros

--- wrappers/node/src/napi_util.h
+++ wrappers/node/src/napi_util.h
@@ -17,21 +17,40 @@ size_t napi_string_length(napi_env env, napi_value string) {
 }
 
 void call_javascript_callback(napi_env env, indy_callback* callback) {
+  #ifdef INDY_LOG_DEBUG
+  printf("entering %s handle %d\n", __func__, callback->handle);
+  #endif
+
   napi_status status;
   napi_value global, err, js_callback;
+
+  #ifdef INDY_LOG_DEBUG
+  printf("acquiring reference to javascript global context\n");
+  #endif
 
   status = napi_get_global(env, &global);
   NAPI_CHECK_STATUS_VOID("napi_get_global");
 
+  #ifdef INDY_LOG_DEBUG
+  printf("acquiring reference to javascript function\n");
+  #endif
   napi_ref callback_ref = callback->callback_refs.at(0);
   status = napi_get_reference_value(env, callback_ref, &js_callback);
   NAPI_CHECK_STATUS_VOID("napi_get_reference_value");
 
   if (callback->multiple == false) {
+    #ifdef INDY_LOG_DEBUG
+    printf("handle %d may not invoke its callback N times\n", callback->handle);
+    printf("deleting reference to javascript function for handle %d\n", callback->handle);
+    #endif
+
     status = napi_delete_reference(env, callback_ref);
     NAPI_CHECK_STATUS_VOID("napi_delete_reference");
   }
 
+  #ifdef INDY_LOG_DEBUG
+  printf("creating javascript number value to represent libindy error for handle %d\n", callback->handle);
+  #endif
   status = napi_create_number(env, (double) callback->error, &err);
   NAPI_CHECK_STATUS_VOID("napi_create_number");
   
@@ -44,8 +63,7 @@ void call_javascript_callback(napi_env env, indy_callback* callback) {
   
   napi_value argv[argc];
 
-  // bind error value
-  argv[0] = err;
+  argv[0] = err; // node's conventional error-first
   argc_idx += 1;
 
   // FIXME
@@ -55,7 +73,10 @@ void call_javascript_callback(napi_env env, indy_callback* callback) {
 
   for (indy_handle_t handle : callback->handle_results) {
     napi_value res;
-    printf("TRACE %d binding number arg %d\n", callback->handle, handle);
+
+    #ifdef INDY_LOG_DEBUG
+    printf("binding number arg %d for handle %d\n", handle, callback->handle);
+    #endif
 
     status = napi_create_number(env, (double) handle, &res);
     NAPI_CHECK_STATUS_VOID("napi_create_number");
@@ -66,8 +87,11 @@ void call_javascript_callback(napi_env env, indy_callback* callback) {
 
   for (char* char_res : callback->char_results) {
     napi_value res;
-    printf("TRACE %d binding string arg %s\n", callback->handle, char_res);
-    
+
+    #ifdef INDY_LOG_DEBUG
+    printf("binding string arg %s for handle %d\n", char_res, callback->handle);
+    #endif
+
     status = napi_create_string_utf8(env, char_res, strlen(char_res), &res);
     NAPI_CHECK_STATUS_VOID("napi_create_string_utf8");
     
@@ -78,7 +102,10 @@ void call_javascript_callback(napi_env env, indy_callback* callback) {
   for (indy_bool_t bool_result : callback->bool_results) {
     napi_value num;
     napi_value res;
-    printf("TRACE %d binding boolean arg %d\n", callback->handle, bool_result);
+
+    #ifdef INDY_LOG_DEBUG
+    printf("binding boolean arg %d for handle %d\n", bool_result, callback->handle);
+    #endif
     
     status = napi_create_number(env, (double) bool_result, &num);
     NAPI_CHECK_STATUS_VOID("napi_create_number");
@@ -90,26 +117,49 @@ void call_javascript_callback(napi_env env, indy_callback* callback) {
     argc_idx += 1;
   }
 
+  #ifdef INDY_LOG_DEBUG
+  printf("calling into javascript for handle %d\n", callback->handle);
+  #endif
   status = napi_make_callback(env, global, js_callback, argc_idx, argv, NULL);
   NAPI_CHECK_STATUS_VOID("napi_make_callback");
 }
 
 void execute(napi_env env, void* data) {
   indy_callback* callback = (indy_callback*) data;
-  if (!callback) return;
-  printf("TRACE execute %d\n", callback->handle);
+  if (!callback) {
+    #ifdef INDY_LOG_WARN
+    printf("entering %s but context data is NULL\n", __func__);
+    #endif
+    return;
+  }
+
+  #ifdef INDY_LOG_DEBUG
+  printf("entering %s for handle %d\n", __func__, callback->handle);
+  #endif
 
   while (true) {
     // construct a lock
     std::unique_lock<std::mutex> lock(callback->mutex);
     
+    #ifdef INDY_LOG_DEBUG
+    printf("constructed lock for handle %d\n", callback->handle);
+    #endif
+
     // and wait to be notified of new data
     callback->cv.wait(lock, [callback] {
       return callback->cancelled || callback->completed;
     });
 
+    #ifdef INDY_LOG_DEBUG
+    printf("got wakeup signal for handle %d\n", callback->handle);
+    #endif
+
     // if the current callback may be called N times
     if (callback->multiple) {
+
+      #ifdef INDY_LOG_DEBUG
+      printf("handle %d may invoke its callback multiple times\n", callback->handle);
+      #endif
 
       // call into javascript with the latest results
       call_javascript_callback(env, callback);
@@ -117,14 +167,21 @@ void execute(napi_env env, void* data) {
       // if the results should be used to schedule a new thread
       if (callback->creates_new_callback_from_results == true) {
         
+        #ifdef INDY_LOG_DEBUG
+        printf("handle %d schedules new callbacks from its results\n", callback->handle);
+        #endif
+
         indy_callback* next_callback;
         size_t libindy_method_length = strlen(callback->libindy_method);
         if (strncmp(callback->libindy_method, "agent_listen_connection", libindy_method_length) == 0) {
+
+          #ifdef INDY_LOG_DEBUG
+          printf("constructing new indy_callback struct from handle %d for agent_listen_connection\n", callback->handle);
+          #endif
+
           // construct a new callback that can be called multiple
           // times but may not schedule new threads with its results
           next_callback = new_callback_from_existing(env, callback, true, false);
-          
-          // we actually dont need to name it because we won't reschedule with results
           next_callback->libindy_method = "agent_listen_message";
         } else {
           // if we got here, it's definitely programmer error
@@ -137,6 +194,10 @@ void execute(napi_env env, void* data) {
         // add to global map
         set_callback(next_callback);
         
+        #ifdef INDY_LOG_DEBUG
+        printf("scheduling new callback from handle %d for %s\n", next_callback->handle, next_callback->libindy_method);
+        #endif
+
         // schedule the new callback for execution
         napi_status status;
         napi_async_work work;
@@ -158,7 +219,9 @@ void execute(napi_env env, void* data) {
 
     // unlock
     lock.unlock();
-    return;
+
+    // and leave
+    break;
   }
 }
 
@@ -171,21 +234,33 @@ void complete(
     // NOTE
     // we have to check this first as the struct associated
     // with this task will already have been deallocated
-    printf("============================ CANCELLED\n");
+    #ifdef INDY_LOG_INFO
+    printf("entering %s but task is cancelled\n", __func__);
+    #endif
     return;
   }
 
   indy_callback* callback = (indy_callback*) data;
   if (!callback) {
-    printf("============================= NULL\n");
+    #ifdef INDY_LOG_WARN
+    printf("entering %s but context data is NULL\n", __func__);
+    #endif
     return;
   }
 
-  printf("TRACE complete %d\n", callback->handle);
+  #ifdef INDY_LOG_DEBUG
+  printf("entering %s for handle %d\n", __func__, callback->handle);
+  #endif
 
   if (callback->cancelled == true) {
-    printf("========================== CANCELLED %d\n", callback->handle);
-    printf("========================== FREEING CALLBACK %d\n", callback->handle);
+    #ifdef INDY_LOG_INFO
+    printf("handle %d was cancelled\n", callback->handle);
+    #endif
+    
+    #ifdef INDY_LOG_DEBUG
+    printf("handle %d is being deallocated\n", callback->handle);
+    #endif
+
     free_callback(callback->handle);
     return;
   }
@@ -196,20 +271,43 @@ void complete(
 
   if (callback->creates_new_callback_from_results == true) {
 
+    #ifdef INDY_LOG_DEBUG
+    printf("handle %d schedules new callbacks from its results\n", callback->handle);
+    #endif
+
     indy_callback* next_callback;
     size_t libindy_method_length = strlen(callback->libindy_method);
 
     if (strncmp(callback->libindy_method, "agent_listen_listening", libindy_method_length) == 0) {
+      
+      #ifdef INDY_LOG_DEBUG
+      printf("constructing new indy_callback struct from handle %d for agent_listen_connection\n", callback->handle);
+      #endif
+      
       next_callback = new_callback_from_existing(env, callback, true, true);
       next_callback->libindy_method = "agent_listen_connection";
     } else if (strncmp(callback->libindy_method, "agent_connect_connect", libindy_method_length) == 0) {
+      
+      #ifdef INDY_LOG_DEBUG
+      printf("constructing new indy_callback struct from handle %d for agent_connect_message\n", callback->handle);
+      #endif
+
       next_callback = new_callback_from_existing(env, callback, true, false);
+      next_callback->libindy_method = "agent_connect_message";
     } else {
+
+      // if we got here it can only be programmer error
+      // as every string comparison case is hardcoded based
+      // on the behaviour of libindy calls
       printf("================================= FATAL didnt match a branch for next callback type\n");
       exit(1);
     }
 
     set_callback(next_callback);
+
+    #ifdef INDY_LOG_DEBUG
+    printf("scheduling new callback from handle %d for %s\n", next_callback->handle, next_callback->libindy_method);
+    #endif
 
     napi_async_work work;
     status = napi_create_async_work(env, execute, complete, next_callback, &work);
