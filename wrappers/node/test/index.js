6ba3c69f23c0cc930571db8822ac00d98d7c7ab6
Ant Cosentino <antony@mycosentino.com>
2017-08-22 15:38:44 +0200

    run the bindings tests

--- wrappers/node/test/index.js
+++ wrappers/node/test/index.js
@@ -21,23 +21,23 @@ describe('libindy', function() {
     clearInterval(block_timer)
   })
 
-  // describe('bindings', function() {
-  //   var funcs = Object.keys(indy)
-  //   funcs.splice(funcs.indexOf('errors'), 1)
+  describe('bindings', function() {
+    var funcs = Object.keys(indy)
+    funcs.splice(funcs.indexOf('errors'), 1)
 
-  //   funcs.forEach(function(func) {
-  //     describe(func, function() {
-  //       it('should throw if given no arguments', function(done) {
-  //         try {
-  //           indy[func]()
-  //         } catch (e) {
-  //           return done()
-  //         }
-  //         return done(new Error(`${func_name} did not throw`))
-  //       })
-  //     })
-  //   })
-  // })
+    funcs.forEach(function(func) {
+      describe(func, function() {
+        it('should throw if given no arguments', function(done) {
+          try {
+            indy[func]()
+          } catch (e) {
+            return done()
+          }
+          return done(new Error(`${func_name} did not throw`))
+        })
+      })
+    })
+  })
 
   it('concurrency', function(done) {
     var calls = 2
