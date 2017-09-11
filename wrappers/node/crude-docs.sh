e02e542db0c2817fef7c61d45a1ba15065baafe2
el-patron <el.patron@korsimoro.com>
2017-09-11 11:02:48 +0700

    removing extraneous files, making simple test, which fails due to inability to point to library directly before configuration - can be updated with some more sophisticated testing, to locate the library and udpate LD_LIBRARY_PATH before the tests

--- wrappers/node/crude-docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf docs
-typedoc --out docs --mode modules --includeDeclarations --excludeExternals --entryPoint LibIndy --ignoreCompilerErrors src
---
+++ wrappers/node/test/main.ts
@@ -0,0 +1,9 @@
+import { describe,it } from 'mocha'
+
+
+describe('The Foreign Function Interface, ', function() {
+  it('should load',function() {
+    const libindy = require('../src')
+  })
+
+});
